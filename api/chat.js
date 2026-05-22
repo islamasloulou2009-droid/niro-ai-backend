64bed33fd5eea52b41f412e2c08e8fe3bccc3517
islamasloulou2009-droid <islamasloulou2009@gmail.com>
2026-05-22 02:39:29 +0100

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
         'X-Title': 'Niro AI'
       },
       body: JSON.stringify({
-        model: 'deepseek/deepseek-chat-v3-0324:free',
+        model: 'deepseek/deepseek-v4-flash:free',
         messages: [
           {
             role: 'system',
